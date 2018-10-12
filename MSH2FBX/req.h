1562b27a11c0cdb1dd776ce31539ba9166f16c6f
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-10-13 01:02:49 +0200

    Introducing first stage of project

---
+++ MSH2FBX/req.h
@@ -0,0 +1,22 @@
+// This file should include all requirements
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include <type_traits>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+
+namespace MSH2FBX
+{
+	using std::clamp;
+	using std::string;
+	using std::vector;
+	using std::queue;
+	using std::unique_ptr;
+	using std::function;
+}
