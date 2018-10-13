8001c2ec0bc3937fa796053e57f9613e9c4bf643
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-10-13 22:45:24 +0200

    Some small refactoring (Converter methods got its own static class)

---
+++ MSH2FBX/MSH2FBX.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "pch.h"
+
+namespace MSH2FBX
+{
+	using namespace LibSWBF2::Logging;
+	using namespace LibSWBF2::Chunks::Mesh;
+	using namespace LibSWBF2::Types;
+
+	void Log(const char* msg);
+	void Log(const string& msg);
+	void LogEntry(LoggerEntry entry);
+}
