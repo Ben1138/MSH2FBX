5c06cc73b3a806f86ba9589d7db8e8b91413c0b2
Ben1138 <marten.mildt@web.de>
2018-11-10 16:00:11 +0100

    Added progressbar

--- MSH2FBX/MSH2FBX.h
+++ MSH2FBX/MSH2FBX.h
@@ -7,9 +7,13 @@ namespace MSH2FBX
 	using namespace LibSWBF2::Chunks::Mesh;
 	using namespace LibSWBF2::Types;
 
+	static bool IsInProgress = false;
+
 	void Log(const char* msg);
 	void Log(const string& msg);
 	void LogEntry(LoggerEntry entry);
+	void ShowProgress(const float progress);
+	void FinishProgress();
 
 	string GetFileName(const string& path);
 	string RemoveFileExtension(const string& fileName);
