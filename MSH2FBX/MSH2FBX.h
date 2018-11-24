dbaa13a792809ceb5fd035d0e161212eecb28b5b
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2018-11-24 08:23:22 +0100

    WIP

--- MSH2FBX/MSH2FBX.h
+++ MSH2FBX/MSH2FBX.h
@@ -4,16 +4,14 @@
 
 namespace MSH2FBX
 {
-	using namespace LibSWBF2::Logging;
 	using namespace LibSWBF2::Chunks::Mesh;
-	using namespace LibSWBF2::Types;
+	using Converter::EChunkFilter;
 	namespace fs = std::filesystem;
 
 	static bool IsInProgress = false;
 
 	void Log(const char* msg);
 	void Log(const string& msg);
-	void LogEntry(LoggerEntry entry);
 	void ShowProgress(const string& text, const float progress);
 	void FinishProgress();
 
